cc9c0be067228dc39c9215dcb64d42660543d394
JP <jp.mathieu.r2001@gmail.com>
2026-04-07 14:34:42 -0400

    Primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PackagePIP",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="JPMR2001",                         # Tu nombre
+    author_email="jp.mathieu.r2001@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/JPMR2001/PackagePIP",     # URL del proyecto
+)
